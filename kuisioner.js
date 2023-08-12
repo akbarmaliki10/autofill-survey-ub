a8c61ca05f94094d5edd0df0381aea3824759233
Akbar Maliki <akbarmaliki109@gmail.com>
2023-08-12 16:50:24 +0700

    fix login bug

--- kuisioner.js
+++ kuisioner.js
@@ -1,19 +1,31 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+const PCR = require("puppeteer-chromium-resolver");
 const prompt = require("prompt-sync")();
 
+var nim;
+var password;
+const delay = (milliseconds) => new Promise((resolve) => setTimeout(resolve, milliseconds));
+
 async function main() {
+    const options = {};
+    
+    const stats = await PCR(options);
     console.log("Welcome to Autofill SIAM UB Survey");
     console.log("Its IMPORTANT to have Chrome Installed!");
-    const browser = await puppeteer.launch({
-        headless: false
+    const browser = await stats.puppeteer.launch({
+        headless: false,
+        args: ["--no-sandbox"],
+        executablePath: stats.executablePath
+    }).catch(function(error) {
+        console.log(error);
     });
     var page = await browser.newPage();
     await page.goto('https://siam.ub.ac.id/');
 
 
     console.log("Press CTRL + C or close browser windows to exit the program")
-    const nim = prompt("Please insert your nim : ");
-    const password = prompt("Please insert your password : ");
+    nim = prompt("Please insert your nim : ");
+    password = prompt("Please insert your password : ");
     await page.type('input[name|="username"]', nim);
     await page.type('input[name|="password"]', password);
     await page.evaluate(() => {
@@ -42,7 +54,33 @@ async function main() {
 
     for (let index = 0; index < allForm; index++) {
         page = await browser.newPage();
-        await page.goto("https://siam.ub.ac.id/notifikasi.php")
+        const response = await page.goto("https://siam.ub.ac.id/notifikasi.php")
+
+        const chain = response.request().redirectChain();
+        console.log(chain)
+
+        if (index%4 == 0) {
+            console.log("-----------------Relogin-----------------")
+            await page.goto("https://siam.ub.ac.id/logout.php")
+            page.close()
+            page = await browser.newPage();
+            await page.goto('https://siam.ub.ac.id/');
+            await page.waitForSelector("input")
+            await delay(2000);
+        
+        
+            console.log("Press CTRL + C or close browser windows to exit the program")
+            await page.type('input[name|="username"]', nim);
+            await page.type('input[name|="password"]', password);
+            console.log(nim,password)
+            await page.evaluate(() => {
+                document.querySelector('input[type=submit]').click();
+            });
+            await page.close()
+            page = await browser.newPage();
+            await page.goto("https://siam.ub.ac.id/notifikasi.php")
+        }
+
         await page.waitForSelector("tr.text td a")
         let surveyTitle = await page.evaluate(async () => {
             return document.querySelector('tr.text td[align]').innerHTML
@@ -62,6 +100,8 @@ async function main() {
 
         page = await browser.newPage();
         await page.goto("https://siam.ub.ac.id/" + hrefSurvey)
+        console.log(hrefSurvey)
+        await page.waitForSelector("tr.text td input")
         await page.evaluate(() => {
             var radio = document.querySelectorAll('tr.text td input[value|="5"]')
             for (let index = 0; index < radio.length; index++) {
@@ -78,4 +118,10 @@ async function main() {
     console.log("Succesfully fill survey. Success:" + formFilled.toString() + " Failed: " + (allForm - formFilled).toString())
     await browser.close();
 }
+
+
+function login() {
+
+}
+
 main();
