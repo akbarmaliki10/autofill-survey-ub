d8c1e045b7ef9517f115063eac5e3aa0febefcfe
Akbar Maliki Haqoni Jati <akbar.maliki@ui.ac.id>
2023-08-12 12:02:21 +0700

    add input prompt for better ux

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # autofill-survey-ub
 A program that can auto fill survey in SIAM UB
+
+## To make executable file you can run
+1. Create Executable file
+```
+pkg --compress GZip package.json
+```
+2. Run Executable File (For Windows)
+```
+./dist/autofill-survey-siam-ub-win
+```
--- kuisioner.js
+++ kuisioner.js
@@ -1,51 +1,46 @@
 const puppeteer = require('puppeteer');
-
-// const listForm = document.querySelectorAll('tr.text td a')
-// for (let index = 0; index < listForm.length; index++) {
-//     console.log(listForm[index].getAttribute("href"))
-// }
-
-
-// document.querySelectorAll('tr.text td a')[0].click()
-// window.addEventListener('load', function () {
-//     alert("It's loaded!")
-//   })
-// var radio = document.querySelectorAll('tr.text td input[value|="5"]')
-// for (let index = 0; index < radio.length; index++) {
-//     radio[index].checked = true
-// }
-// document.getElementById("saran").value = "terima kasih semoga berkah"
-// document.getElementById("form_kuis").submit()
-
-const nim = "205070100111010"
-const password = "127Azzam?"
-
+const prompt = require("prompt-sync")();
 
 async function main() {
+    console.log("Welcome to Autofill SIAM UB Survey");
+    console.log("Its IMPORTANT to have Chrome Installed!");
     const browser = await puppeteer.launch({
         headless: false
     });
     var page = await browser.newPage();
     await page.goto('https://siam.ub.ac.id/');
 
+
+    console.log("Press CTRL + C or close browser windows to exit the program")
+    const nim = prompt("Please insert your nim : ");
+    const password = prompt("Please insert your password : ");
     await page.type('input[name|="username"]', nim);
     await page.type('input[name|="password"]', password);
     await page.evaluate(() => {
         document.querySelector('input[type=submit]').click();
     });
     page.close()
-    
+
     page = await browser.newPage();
-    await page.goto("https://siam.ub.ac.id/notifikasi.php");
+    const response = await page.goto("https://siam.ub.ac.id/notifikasi.php");
+    const chain = response.request().redirectChain();
+    console.log(chain.length); // Return 1
+    if (chain.length > 0) {
+        console.log("Your Login Credentials is FALSE! Stopping program immediately")
+        page.close()
+        await browser.close();
+        return;
+    }
+
     var allForm = await page.evaluate(() => {
         return document.querySelectorAll('tr.text td a').length
     })
     let formFilled = 0;
-    console.log("Survey to be filled: "+allForm.toString())
+    console.log("Survey to be filled: " + allForm.toString())
     console.log("-------------------------------------------------------------------------------------------------------")
     page.close()
-    
-    for (let index = 0; index < 2; index++) {
+
+    for (let index = 0; index < allForm; index++) {
         page = await browser.newPage();
         await page.goto("https://siam.ub.ac.id/notifikasi.php")
         await page.waitForSelector("tr.text td a")
@@ -66,7 +61,7 @@ async function main() {
         page.close()
 
         page = await browser.newPage();
-        await page.goto("https://siam.ub.ac.id/"+hrefSurvey)
+        await page.goto("https://siam.ub.ac.id/" + hrefSurvey)
         await page.evaluate(() => {
             var radio = document.querySelectorAll('tr.text td input[value|="5"]')
             for (let index = 0; index < radio.length; index++) {
@@ -80,7 +75,7 @@ async function main() {
         console.log("SUCCESS")
     }
     console.log("-------------------------------------------------------------------------------------------------------")
-    console.log("Succesfully fill survey. Success:" + formFilled.toString() + " Failed: "+ (allForm-formFilled).toString())
+    console.log("Succesfully fill survey. Success:" + formFilled.toString() + " Failed: " + (allForm - formFilled).toString())
     await browser.close();
 }
 main();
